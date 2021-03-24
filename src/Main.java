939c5b199a7384859e9bd013684b97e7fc7a7cb8
Oleg Esipov <oleg.esipov@gmail.com>
2021-03-24 12:18:56 +0500

    Inital commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int deposit = 500_000_000;
+        int total = balance + deposit;
+        System.out.println("total = " + total);
+
+    }
+}
